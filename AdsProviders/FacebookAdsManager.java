eab49ecb365a10b7984cbdc9a0594bd113661030
Mahendra-Gohil <42574216+Mahendra-Gohil@users.noreply.github.com>
2021-05-24 13:46:59 +0530

    Update FacebookAdsManager.java

--- AdsProviders/FacebookAdsManager.java
+++ AdsProviders/FacebookAdsManager.java
@@ -1,7 +1,7 @@
 // For Facebook Ads PlatForm
 //implementation 'com.facebook.android:audience-network-sdk:6.4.0'
 
-package com.maxfour.music.ads.AdsProviders;
+package package com.demo.ads.AdsProviders;
 
 import android.app.ProgressDialog;
 import android.content.Context;
